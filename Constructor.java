6867abb8a891c247a838cf5cd97ee692bf6e8914
Shruti Yaligar <shruti.yaligar@kerv.com>
2024-05-18 12:38:48 +0530

    My first java program commit

---
+++ Constructor.java
@@ -0,0 +1,14 @@
+package BasicKnowledge;
+
+public class Constructor {
+	
+	Constructor(){
+		System.out.println("I am a Constructor");
+	}
+
+	public static void main(String[] args) {
+		new Constructor();
+
+	}
+
+}
