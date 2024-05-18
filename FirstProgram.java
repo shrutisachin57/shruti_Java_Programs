7c5d447af246608edf0d60b668c7658794d7d83f
Shruti Yaligar <shruti.yaligar@kerv.com>
2024-05-18 12:56:53 +0530

    my commit

---
+++ FirstProgram.java
@@ -0,0 +1,13 @@
+package BasicKnowledge;
+
+public class FirstProgram {
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("First Program");
+		System.out.println("shruti");
+
+	}
+
+}
